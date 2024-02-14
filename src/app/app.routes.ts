be8b0d86517e20d927fe7df184360d38c971a4da
Hanny-cracker <tembindakwa@gmail.com>
2024-02-14 13:54:16 +0100

    added styles to the service pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
+import { config } from './app.config.server';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import {AboutUsComponent} from './pages/about-us/about-us.component';
 import { OurHistoryComponent } from './pages/our-history/our-history.component';
 import { TeamMembersComponent } from './pages/team-members/team-members.component';
 import { ContactComponent } from './contact/contact.component';
+import { BuildingConstructionComponent } from './services/building-construction/building-construction.component';
+import { BuildingDesignComponent } from './services/building-design/building-design.component';
+import { PropertyMaintenanceComponent } from './services/property-maintenance/property-maintenance.component';
+import { HomeConstructionComponent } from './services/home-construction/home-construction.component';
 
 
 export const routes: Routes = [
@@ -23,17 +28,17 @@ export const routes: Routes = [
         title: 'Home page'
       },
       {
-        path: 'about-us',
+        path: 'pages/about-us',
         component: AboutUsComponent,
         title: 'About Us'
       },
       {
-        path: 'team',
+        path: 'pages/team',
         component: TeamMembersComponent,
         title: 'About Us'
       },
       {
-        path: 'history',
+        path: 'pages/history',
         component: OurHistoryComponent,
         title: 'About Us'
       },
@@ -41,5 +46,25 @@ export const routes: Routes = [
         path: 'contact',
         component: ContactComponent,
         title: 'Contact Us'
+      },
+      {
+        path : 'services/building construction',
+        component : BuildingConstructionComponent,
+        title : 'Bulding Construction'
+      },
+      {
+        path : 'services/design',
+        component :  BuildingDesignComponent,
+        title : 'Building Design'
+      },
+      {
+        path : 'services/construction',
+        component : HomeConstructionComponent,
+        title : 'Home Construction'
+      },
+      {
+        path : 'services/maintenance',
+        component : PropertyMaintenanceComponent,
+        title : 'Property Maintenace'
       }
 ];
--- src/app/services/building-construction/building-construction.component.ts
+++ src/app/services/building-construction/building-construction.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-building-construction',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './building-construction.component.html',
   styleUrl: './building-construction.component.css'
 })
 export class BuildingConstructionComponent {
+  title = "Building Construction";
 
 }
