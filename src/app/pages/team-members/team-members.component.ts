cc709910e19f6e40a060c4e38f37037058d8dae2
Hanny-cracker <tembindakwa@gmail.com>
2024-02-14 10:13:51 +0100

    added content to the our team page

--- src/app/pages/team-members/team-members.component.ts
+++ src/app/pages/team-members/team-members.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-team-members',
   standalone: true,
-  imports: [],
+  imports: [ RouterModule],
   templateUrl: './team-members.component.html',
   styleUrl: './team-members.component.css'
 })
